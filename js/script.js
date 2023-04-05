43f4fc245199ce1f67227c72cb967409ddd2d0b1
LorenzoSeverini <lorenzo.seve98@gmail.com>
2023-04-05 18:16:17 +0200

    fix(js)

--- js/script.js
+++ js/script.js
@@ -5,6 +5,6 @@ function generatePassword() {
     var name = prompt("What's your name?");
     var surname = prompt("what's your surname?");
     var color = prompt("what's your favorite color:");
-    var passwordText = "name" + surname + color + "2023";
-    document.getElementById("password").value = passwordText ;
+    var passwordText = "name" + "-" + surname + "-" + color + "-" + "2023";
+    document.getElementById("password").value = passwordText;
 }
